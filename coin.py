87a88d1249cac36447b0fae7d0aa4a93c72fd268
Huan-Ting,Chen <dingyiyi0226@gmail.com>
2021-06-24 18:39:53 +0800

    modify coin repr

--- coin.py
+++ coin.py
@@ -4,6 +4,9 @@ class Coin():
         self._private_key = 'pk'
         self._value = 50
 
+    def __repr__(self):
+        return str(self._value)
+
     @property
     def public_key(self):
         return self._public_key
